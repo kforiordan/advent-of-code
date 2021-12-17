045d772591e6daa1292c0100919ea0c2e5fb698a
Kevin O'Riordan <kevin@oriordan.org>
2021-12-17 21:16:28 +0000

    Prints version sum for literal packets.  Woop.
    Learned a bit about classes and type hints though, so that's something,
    right?

--- 16-packet-decoder-01.py
+++ 16-packet-decoder-01.py
@@ -9,28 +9,63 @@ class Header():
         self.version = version
         self.type_id = type_id
 
+    def __repr__(self):
+        return("Header({}, {})".format(self.version, self.type_id))
+
+
+class Payload():
+    nothing: str
+
+    def __repr__(self):
+        return('Payload()')
+
 
 class Packet():
+    header: Header
+    payload: Payload
+
     def __init__(self, header, payload=None):
-        self.header = header
+        if isinstance(header, Header):
+            self.header = header
+        else:
+            if isinstance(header, list):
+                self.header = Header(header[0], header[1])
+            elif isinstance(header, tuple):
+                v, t = header
+                self.header = Header(v, t)
+            else:
+                self.header = "I am not adding exceptions, etc."
+
         if payload != None:
             self.payload = payload
-    #     if sub_packets != None:
-    #         self.sub_packets = sub_packets
-    # , sub_packets=None):
+            if isinstance(payload, list):
+                self.payload = OperatorPayload(payload)
+            else:
+                self.payload = LiteralPayload(payload)
+
+    def __repr__(self):
+        return("Packet(version={}, type_id={}, payload={}".format(
+            self.header.version, self.header.type_id, "something?"))
 
 
-class LiteralPacket(Packet):
-    payload: int
+class LiteralPayload(Packet):
+    v: int
 
     def __init__(self, payload):
-        self.payload = payload
+        self.v = payload
 
-class OperatorPacket(Packet):
-    sub_packets: [Packet]	# Is this right?
+    def __repr__(self):
+        return("LiteralPayload({})".format(self.v))
+
+
+class OperatorPayload(Packet):
+    packets: list[Packet]
 
     def __init__(self, sub_packets):
         self.sub_packets = sub_packets
+
+    def __repr__(self):
+        return("OperatorPayload({})".format("oh man, idk, something"))
 
 
 def parse_header(bitq, idx):
@@ -48,7 +83,7 @@ def parse_header(bitq, idx):
     return idx, Header(version, type_id)
 
 
-def parse_literal_packet_payload(bitq, idx):
+def parse_literal_payload(bitq, idx):
     data_len = 4
 
     numbers = []
@@ -70,21 +105,19 @@ def parse_literal_packet_payload(bitq, idx):
     return idx, literal_goddamn_number
 
 
-def parse_operator_packet(bitq, idx):
-    packets = []
-
+def parse_operator_payload(bitq, idx):
     length_type_id = bitq[idx]
     idx += 1
 
     if length_type_id == '0':
-        packets = parse_operator_packet_type_zero(bitq, idx)
+        idx, payload = parse_operator_payload_type_zero(bitq, idx)
     else:
-        packets = parse_operator_packet_type_zero(bitq, idx)
+        idx, payload = parse_operator_payload_type_one(bitq, idx)
 
-    return idx, packets
+    return idx, payload
 
 
-def parse_operator_packet_type_zero(bitq, idx):
+def parse_operator_payload_type_zero(bitq:list[str], idx:int) -> Payload:
     packets = []
 
     so_sick_of_naming_variables = 15
@@ -101,7 +134,7 @@ def parse_operator_packet_type_zero(bitq, idx):
     return packets
 
 
-def parse_operator_packet_type_one(bitq, idx):
+def parse_operator_payload_type_one(bitq:list[str], idx:int) -> OperatorPayload:
     packets = []
 
     so_sick_of_naming_variables = 11
@@ -115,49 +148,50 @@ def parse_operator_packet_type_one(bitq, idx):
     return idx, packets
 
 
-# Despite the name, this returns a list of packets.
-def parse_packet(bitq, idx, zero_padded=False):
-    packets = []
+# Returns a packet, which may itself contain sub packets.
+def parse_packet(bitq:list[str], idx:int, zero_padded=False) -> Packet:
+    packet = None
     idx, header = parse_header(bitq, idx)
 
     #print(" parse_packet: idx {}, header type {}".format(idx, header.type_id))
+    payload = None
     if header.type_id == 4:
-        idx, packet = parse_literal_packet_payload(bitq, idx)
-        packets.append(packet)
+        parser = parse_literal_payload
     else:
-        idx, many_many_packets = parse_operator_packet(bitq, idx)
-        for packet in many_many_packets:
-            packets.append(packet)
+        parser = parse_operator_payload
+    idx, payload = parser(bitq, idx)
+    packet = Packet(header, payload)
 
+    #pp.pprint(packet)
     if zero_padded:
         n_bits_in_hex = 8  # Yeah, not 16.  Hahahahahah
         n_zeroes = (n_bits_in_hex - (idx % n_bits_in_hex)) % n_bits_in_hex
         for i in range(0, n_zeroes):
             if bitq[idx] != '0':
-                print("You done goofed: {}".format(idx))
+                print("You done goofed: {} {}/{}".format(idx, i, n_zeroes))
             idx += 1
 
-    return idx, packets
+    return idx, packet
 
 
-def parse_bitq(bitq, idx=0):
-    all_packets = []
+def parse_bitq(bitq:list[str], idx:int=0) -> list[Packet]:
+    packets = []
 
     zero_padded = True
 
     n = len(bitq)
     while idx < n:
         #print("parse_bitq {}".format(idx))
-        idx, packets = parse_packet(bitq, idx, zero_padded)
-        for packet in packets:
-            all_packets.append(packet)
+        idx, packet = parse_packet(bitq, idx, zero_padded)
+        packets.append(packet)
 
-    return all_packets
+    return packets
 
 
-def get_packets_as_bit_list(fh):
+def get_packets_as_bit_list(fh) -> list[str]:
     # Read hex from stdin, convert to bits and append to bitq
     for line in sys.stdin:
+        #print(line)
         for c in list(line.strip()):
             b = bin(int(c, base=16))
             bits = list(b)[2:]	# Skip leading '0b'
@@ -168,9 +202,15 @@ def get_packets_as_bit_list(fh):
 
 if __name__ == "__main__":
     pp = pprint.PrettyPrinter()
-    bitq = []	# Yes, I know python lists are poor queues.
+    bitq = []	# Not really a queue
     packets = []
 
     get_packets_as_bit_list(sys.stdin)	# Updates bitq.  Ugh.
 
     packets = parse_bitq(bitq)
+
+    version_sum = 0
+    for p in packets:
+        version_sum += p.header.version
+
+    print("Version sum: {}".format(version_sum))
