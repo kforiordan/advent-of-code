6ac77d9a111ef396194407c637b7a4694831cc3b
Kevin O'Riordan <kevin@oriordan.org>
2022-01-08 17:11:09 +0000

    as good a time to commit as any
    I have a trivial test function written, and some not-quite-correct
    explode functionality

--- 18-snailfish/part-1.py
+++ 18-snailfish/part-1.py
@@ -60,26 +60,89 @@ def split_number(n):
     #
     return("[{},{}]".format(round((n-0.25)/2), round((n+0.25)/2)))
 
-def split(s):
 
+def explode_leftward(s, x):
+    new_left = []
     n = None
+    just_sail_on_through = False
+    for i,c in enumerate(reversed(s)):
+        if just_sail_on_through:
+            new_left.append(c)
+        else:
+            if c in '0123456789':
+                if n == None:
+                    n = int(c)
+                else:
+                    n = (n * 10) + int(c)
+            else:
+                if n == None:
+                    new_left.append(c)
+                else:
+                    new_left.append('{}'.format(x + n))
+    return(''.join(reversed(new_left)))
+
+
+def explode_rightward(s, x):
+    return(''.join(reversed(explode_leftward(s, x))))
+
+
+def explode_or_split(s):
+
+    # For tracking whether we should explode or not
+    explosion_threshold = 4
+    depth = 0
+
+    # For tracking numbers as encountered - split may be needed
+    split_threshold = 10
+    n = None
+
+    # As we move from left to right through the string, we copy stuff leftwards
     left = []
+
+    # We start with the whole string to our right
     right = list(s)
+
+    # After a split or explode we just copy everything from right to left.
     just_sail_on_through = False
 
     for i,c in enumerate(right):
+        #print('{}: "{}" "{}"'.format(i, s[0:i], c))
         if just_sail_on_through:
             left.append(c)
         else:
-            if c in '[],':
-                if n != None:	# if n is a number
-                    if n < 10:
-                        left.append(str(n))
-                    elif n >= 10:
+            if c == '[':
+                depth += 1
+                if depth > explosion_threshold:
+                    left.append('0')
+                else:
+                    left.append(c)
+            elif c == ',':
+                # We need to process the left number of a pair
+                if depth > explosion_threshold:
+                    left = list(explode_leftward(left, n))
+                else:
+                    if n != None:
+                        if n > split_threshold:
+                            left.append(str(split_number(n)))
+                            just_sail_on_through = True
+                        else:
+                            left.append(str(n))
+                        n = None
+                    left.append(c)
+            elif c == ']':
+                # We need to process the right number of a pair
+                if depth > explosion_threshold:
+                    right = list(explode_rightward(right, n))
+                elif n != None:
+                    if n > split_threshold:
                         left.append(str(split_number(n)))
-                        just_sail_on_through = True	# No further processing
+                        left.append(c)
+                        just_sail_on_through = True
+                    else:
+                        left.append(str(n))
+                        left.append(c)
                     n = None
-                left.append(c)
+                depth -= 1
             elif c in '0123456789':
                 if n == None:
                     n = int(c)
@@ -90,21 +153,50 @@ def split(s):
 
 
 def reduce(s):
-    i = 0
     r = s
     while True:
-        r = explode(r)
+        r = explode_or_split(r)
         if r == s:
-            r = split(r)
-            if r == s:
-                break
-    return r
+            break
+    return(r)
+
+
+def trivial_tests():
+    tests = [
+        {
+            'input': '[[[[0,7],4],[15,[0,13]]],[1,1]]',
+            'function': explode_or_split,
+            'expected_output': '[[[[0,7],4],[[7,8],[0,13]]],[1,1]]'
+        },
+        {
+            'input': '[[[[[9,8],1],2],3],4]',
+            'function': explode_or_split,
+            'expected_output': '[[[[0,9],2],3],4]'
+        }
+    ]
+
+    n_passed = 0
+    result_strings = ["failed", "passed"]
+    for i,t in enumerate(tests):
+        actual_output = t['function'](t['input'])
+        result = actual_output == t['expected_output']
+        print("Test {} {}".format(i, result_strings[int(result)]))
+        if not result:
+            print(" Function: {}".format(t['function']))
+            print("      for: {}".format(t['input']))
+            print("   wanted: {}".format(t['expected_output']))
+            print("  but got: {}".format(actual_output))
+        n_passed += result
+    if n_passed == len(tests):
+        print("All {} trivial tests passed!".format(n_passed))
+    else:
+        print("Passed {} out of {} tests :-(".format(n_passed, len(tests)))
+
+    return n_passed == len(tests)
+
 
 
 
 if __name__ == "__main__":
-    a = '[[[[0,7],4],[15,[0,13]]],[1,1]]'
-    a_bis = '[[[[0,7],4],[[7,8],[0,13]]],[1,1]]'
 
-    print(split(a))
-    print(a_bis)
+    trivial_tests()
