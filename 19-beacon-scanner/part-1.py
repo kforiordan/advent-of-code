36ab9561644700721ce3d9aa302a12bfc89eb15c
Kevin O'Riordan <kevin@oriordan.org>
2022-02-02 00:50:29 +0000

    trivial: add a stub find_beacons function
    I think this can be solved by working out the distances between points,
    and comparing those to the distances for other scanners.  No, wait,
    that's O(n^4) or something.  Hmm.

--- 19-beacon-scanner/part-1.py
+++ 19-beacon-scanner/part-1.py
@@ -2,6 +2,7 @@ import sys
 import pprint
 import re
 
+# reports[id] = { orientation: 'xyz', points = [...] }
 def get_reports(fh):
     reports = []
     scanner_id = None
@@ -13,13 +14,19 @@ def get_reports(fh):
         else:
             m = header.match(line)
             if m is None:
-                reports[scanner_id].append(line.split(','))
+                reports[scanner_id]['points'].append(line.split(','))
             else:
                 scanner_id = int(m.group(1))
-                reports.append([])
+                reports.append({'scanner_id': scanner_id,
+                                'orientation': None,
+                                'points': []})
     return reports
+
+def find_beacons(reports, r=1000, req_match_count=12):
+    return []
 
 if __name__ == "__main__":
     pp = pprint.PrettyPrinter()
     reports_by_scanner = get_reports(sys.stdin)
-    pp.pprint(reports_by_scanner)
+    beacons = find_beacons(reports_by_scanner)
+    pp.pprint(beacons)
