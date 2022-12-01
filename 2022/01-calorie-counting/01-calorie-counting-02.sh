1fc18d9e817ccd505efd33eb221d9b19cac13a17
Kevin O'Riordan <kevin@oriordan.org>
2022-12-01 15:10:26 +0000

    A note on dc behaviour.

--- 2022/01-calorie-counting/01-calorie-counting-02.sh
+++ 2022/01-calorie-counting/01-calorie-counting-02.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# Requires GNU dc.  On macos, + with only one number on the stack pops and
+# discards that number, and it's lost.  GNU dc leaves the stack unchanged.  I
+# don't know which behaviour is correct, but I'm relying on GNU's.
+
 sed 's/$/ +/;s/^[ +]*$/pc/' | dc 2>/dev/null \
   | sort -n | tail -n 3 \
   | (sed 's/$/ +/';echo p;) | dc 2>/dev/null
