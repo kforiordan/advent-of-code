9df44284550665e7a400ab9f70cda25bb3ea4b50
Kevin O'Riordan <git@gitlab.com>
2022-12-04 08:23:41 +0000

    Solve part 1 for day 4

--- 2022/04-camp-cleanup/04-camp-cleanup-01.py
+++ 2022/04-camp-cleanup/04-camp-cleanup-01.py
@@ -4,13 +4,19 @@ from functools import reduce
 
 import sys
 
+# returns True if a1 fully contains a2
+def contains(assignment1, assignment2):
+    (a1a, a1z), (a2a, a2z) = assignment1, assignment2
+    return a1a <= a2a and a1z >= a2z
+
 # Reads lists of chars from fh, returns list of tuples, each tuple
 # containing half of a list of chars.
 def get_assignments(fh):
     assignments = []
 
     for line in fh:
-        a1,a2 = map(lambda a: tuple(a.split("-")), line.split(","))
+        line = line.rstrip('\n')
+        a1,a2 = map(lambda a: tuple(map(int, a.split("-"))), line.split(","))
         assignments.append((a1,a2))
 
     return assignments
@@ -19,5 +25,9 @@ def get_assignments(fh):
 if __name__ == "__main__":
     assignments = get_assignments(sys.stdin)
 
-    silver = len(assignments)
-    print(f'Silver: {silver}')
+    n = 0
+    for (a1,a2) in assignments:
+        if contains(a1, a2) or contains(a2, a1):
+            n += 1
+
+    print(f'Silver: {n}')
