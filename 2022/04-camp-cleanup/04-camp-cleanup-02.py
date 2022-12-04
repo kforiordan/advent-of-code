963c0d0afffd8d979ed59f0e5efc314cfe52d3bb
Kevin O'Riordan <git@gitlab.com>
2022-12-04 11:42:47 +0000

    solve day 4, puzzle 2

---
+++ 2022/04-camp-cleanup/04-camp-cleanup-02.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+from functools import reduce
+
+import sys
+
+# returns True if a1 overlaps a2
+def overlaps(assignment1, assignment2):
+    (a1a, a1z), (a2a, a2z) = assignment1, assignment2
+    return (a1a >= a2a and a1a <= a2z) or (a1z >= a2a and a1z <= a2z)
+
+# returns True if a1 fully contains a2
+def contains(assignment1, assignment2):
+    (a1a, a1z), (a2a, a2z) = assignment1, assignment2
+    return a1a <= a2a and a1z >= a2z
+
+# Reads lists of chars from fh, returns list of tuples or tuples ...
+def get_assignments(fh):
+    assignments = []
+
+    for line in fh:
+        line = line.rstrip('\n')
+        a1,a2 = map(lambda a: tuple(map(int, a.split("-"))), line.split(","))
+        assignments.append((a1,a2))
+
+    return assignments
+
+
+if __name__ == "__main__":
+    assignments = get_assignments(sys.stdin)
+
+    n = 0
+    for (a1,a2) in assignments:
+        if contains(a1, a2) or overlaps(a1, a2):
+            n += 1
+
+    print(f'Gold: {n}')
