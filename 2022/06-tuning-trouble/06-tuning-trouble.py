d26c3a3114bf0770730072fcba63132776df4f5a
Kevin O'Riordan <git@gitlab.com>
2022-12-06 10:32:51 +0000

    trimmed 68 lines down to 55
    Many people's solutions were about half the length of mine, or less.

--- 2022/06-tuning-trouble/06-tuning-trouble.py
+++ 2022/06-tuning-trouble/06-tuning-trouble.py
@@ -4,13 +4,7 @@ import sys
 
 
 def get_message_buffers(fh):
-    message_buffers = []	# Test data has four messages, prod has one.
-
-    for line in fh:
-        line = line.rstrip('\n')
-        message_buffers.append(line)
-
-    return message_buffers
+    return [line.rstrip('\n') for line in fh]
 
 
 def is_unique(s, flag_len=4):
@@ -18,17 +12,15 @@ def is_unique(s, flag_len=4):
         print(f'flag length is {len(s)}, should be {flag_len}')
         return False
 
-    i=0
     dup_found=False
+    i=0
     while i < flag_len:
-
         j=i+1
         while j < flag_len:
             if s[i] == s[j]:
                 dup_found = True
                 break
             j += 1
-
         if dup_found:
             break
         i += 1
@@ -37,7 +29,6 @@ def is_unique(s, flag_len=4):
 
 
 def find_start(message_buffer, flag_len=4):
-
     message_start_index = None
     buffer_len = len(message_buffer)
     i = 0
@@ -51,18 +42,14 @@ def find_start(message_buffer, flag_len=4):
 
 
 if __name__ == "__main__":
-
     message_buffers = get_message_buffers(sys.stdin)
-
     silver_flag_len = 4
     gold_flag_len = 14
 
     for mb in message_buffers:
         start = find_start(mb, silver_flag_len)
-        m = mb[0:13]
-        print(f'Silver: {m}... -> {start}')
+        print(f'Silver: {mb[0:13]}... -> {start}')
 
     for mb in message_buffers:
         start = find_start(mb, gold_flag_len)
-        m = mb[0:13]
-        print(f'Gold: {m}... -> {start}')
+        print(f'Gold: {mb[0:13]}... -> {start}')
