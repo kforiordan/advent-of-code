253607632a0b9aa23182461a8167acfcd2997b3b
Kevin O'Riordan <kevin.oriordan@heanet.ie>
2023-12-03 11:41:59 +0000

    find all numbers and symbols in input

--- 2023/03-gear-ratios/03-gear-ratios.py
+++ 2023/03-gear-ratios/03-gear-ratios.py
@@ -5,5 +5,42 @@ import sys
 def get_schematic(fh):
     return [list(line.strip()) for line in fh]
 
+def discover_numbers_and_symbols(schematic):
+    numbers = []
+    symbols = []
+    noop_char = '.'
+
+    n = {}
+    for y,row in enumerate(schematic):
+        if "x" in n:
+            numbers.append(n)
+            n = {}
+        for x, c in enumerate(row):
+            if c.isdigit():
+                if "x" in n:
+                    n["digits"].append(int(c))
+                else:
+                    n["y"] = y
+                    n["x"] = x
+                    n["digits"] = [int(c)]
+            else:
+                if "x" in n:
+                    numbers.append(n)
+                    n = {}
+                if c != noop_char:
+                    symbols.append({"y":y, "x":x, "symbol":c})
+    if "x" in n:
+        numbers.append(n)
+        n = {}
+
+    return numbers, symbols
+
+
 if __name__ == "__main__":
     schematic = get_schematic(sys.stdin)
+
+    numbers, symbols = discover_numbers_and_symbols(schematic)
+    print(numbers)
+    print(symbols)
+
+
