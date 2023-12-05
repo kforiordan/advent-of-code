5c536713a5bfcf96e2afb6f6c373007e17bddab0
Kevin O'Riordan <kevin.oriordan@heanet.ie>
2023-12-05 20:52:43 +0000

    solve day 3 part 1
    it is now coming the the end of day 5

--- 2023/03-gear-ratios/03-gear-ratios.py
+++ 2023/03-gear-ratios/03-gear-ratios.py
@@ -46,25 +46,35 @@ def discover_numbers_and_symbols(schematic):
     return numbers, symbols
 
 
+# True if the character blah blah is a symbol
 def is_symbol(schematic, y, x):
-    # This is a weak definition of a symbol.
-    return not schematic[y][x].isdigit() and not schematic[y][x] == '.'
+    # This is a very weak definition.
+    return not (schematic[y][x].isdigit() or schematic[y][x] == '.')
 
+# True if x & y are within the bounds of the given schematic
 def in_bounds(schematic, y, x):
     return y >= 0 and y < len(schematic) and x >= 0 and x < len(schematic[0])
 
-def symbol_adjacent(schematic,  y, x):
+# True if there is a symbol adjacent to the given coordinate.
+def symbol_adjacent(schematic, y, x):
     f = lambda y, x: in_bounds(schematic, y, x) and is_symbol(schematic, y, x)
 
-    return f(y-1, x-1) or f(y-1, x) or f(y-1, x+1)
+    return (f(y-1, x-1) or f(y-1, x) or f(y-1, x+1) or
+            f(y, x-1) or f(y, x+1) or
+            f(y+1, x-1) or f(y+1, x) or f(y+1, x+1))
 
 
-def symbol_adjacent_numbers(numbers, symbols):
+def symbol_adjacent_numbers(schematic, numbers, symbols):
+    these_ones = []
+
     for n in numbers:
-        print(n)
+        x,y = n["x"],n["y"]
+        for i,d in enumerate(n["digits"]):
+            if symbol_adjacent(schematic, y, x+i):
+                these_ones.append(n)
+                break
 
-
-    return []
+    return these_ones
 
 
 if __name__ == "__main__":
@@ -72,6 +82,6 @@ if __name__ == "__main__":
 
     numbers, symbols = discover_numbers_and_symbols(schematic)
 
-    good_numbers = symbol_adjacent_numbers(numbers, symbols)
+    good_numbers = symbol_adjacent_numbers(schematic, numbers, symbols)
 
-    print(numbers)
+    print("Silver: {}".format(sum([n["value"] for n in good_numbers])))
