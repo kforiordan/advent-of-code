421ca6ab35a66fedc8a1b4ab3a9c741bcddf69d9
Kevin O'Riordan <kevin.oriordan@heanet.ie>
2023-12-06 21:49:27 +0000

    solve day 4 part 2

--- 2023/04-scratchcards/04-scratchcards.py
+++ 2023/04-scratchcards/04-scratchcards.py
@@ -36,7 +36,20 @@ if __name__ == "__main__":
     scratchcards = get_scratchcards(sys.stdin)
     score = 0
     for s in scratchcards:
-        n_winners = len(my_winners(s["winning_numbers"], s["my_numbers"]))
+        w = my_winners(s["winning_numbers"], s["my_numbers"])
+        s["my_winners"] = w
+        n_winners = len(w)
         if n_winners > 0:
             score += pow(2, n_winners-1)
     print("Silver: {}".format(score))
+
+    rec_wins = [None for _ in range(len(scratchcards))]
+    for i in reversed(range(len(scratchcards))):
+        s = scratchcards[i]
+        rec_wins[i] = 1
+        for j,_ in enumerate(s["my_winners"]):
+            j += 1
+            if i+j < len(scratchcards):
+                rec_wins[i] += rec_wins[i+j]
+
+    print("Gold: {}".format(sum(rec_wins)))
