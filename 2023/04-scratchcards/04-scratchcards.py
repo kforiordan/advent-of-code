7e10cc7bae4cbac9961b08ab536d553bdda82642
Kevin O'Riordan <kevin.oriordan@heanet.ie>
2023-12-06 08:46:26 +0000

    solve day 4 part 1

--- 2023/04-scratchcards/04-scratchcards.py
+++ 2023/04-scratchcards/04-scratchcards.py
@@ -8,18 +8,35 @@ def get_scratchcards(fh):
 
     card_re = re.compile('Card ([ 0-9]+): *([ 0-9]+)\|([ 0-9]+)$')
     for line in fh:
+        card = {}
         line = line.strip()
         m = card_re.match(line)
-        id = m.group(1)
-        winning_numbers = sorted(list(map(int, filter(lambda s: s.isdigit(), m.group(2).split(' ')))))
-        my_numbers = list(map(int, filter(lambda s: s.isdigit(), m.group(3).split(' '))))
-        print(id)
-        print(winning_numbers)
-        print(my_numbers)
-        print("-- ")
+        card["orig_line"] = line
+        card["game_id"] = m.group(1)
+        card["winning_numbers"] = sorted(list(map(int, filter(lambda s: s.isdigit(), m.group(2).split(' ')))))
+        card["my_numbers"] = list(map(int, filter(lambda s: s.isdigit(), m.group(3).split(' '))))
+        cards.append(card)
 
     return cards
 
 
+def my_winners(winning_numbers, my_numbers):
+    wow = []
+
+    for m in my_numbers:
+        for n in winning_numbers:
+            if m == n:
+                wow.append(m)
+                break
+
+    return wow
+
+
 if __name__ == "__main__":
     scratchcards = get_scratchcards(sys.stdin)
+    score = 0
+    for s in scratchcards:
+        n_winners = len(my_winners(s["winning_numbers"], s["my_numbers"]))
+        if n_winners > 0:
+            score += pow(2, n_winners-1)
+    print("Silver: {}".format(score))
