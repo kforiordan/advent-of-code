7d21d0fd3b994f9b5ab56499a9024c99eb0f63ac
Kevin O'Riordan <kevin.oriordan@heanet.ie>
2023-12-10 21:34:44 +0000

    Solve day 09 part 01

--- 2023/09-mirage-maintenance/09-mirage-maintenance.py
+++ 2023/09-mirage-maintenance/09-mirage-maintenance.py
@@ -7,5 +7,31 @@ def get_oasis_reports(fh):
     return [list(map(int, line.rstrip().split(' '))) for line in fh]
 
 
+def get_differences(report):
+    prev = None
+    diffs = []
+    for curr in report:
+        if prev != None:
+            diffs.append(curr - prev)
+        prev = curr
+    return diffs
+
+
+def all_zeroes(diffs):
+    for d in diffs:
+        if d != 0:
+            return False
+    return True
+
+
+def predict_next(report):
+    if all_zeroes(report):
+        return 0
+    else:
+        return report[-1] + predict_next(get_differences(report))
+
+
 if __name__ == "__main__":
-    print(get_oasis_reports(sys.stdin))
+
+    reports = get_oasis_reports(sys.stdin)
+    print("Silver: {}".format(sum(map(predict_next, reports))))
