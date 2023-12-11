5a148036ef2ae05c07936f138c2dac5c751800f1
Kevin O'Riordan <kevin.oriordan@heanet.ie>
2023-12-11 21:32:27 +0000

    committing just to share between machiens

--- 2023/10-pipe-maze/10-pipe-maze.py
+++ 2023/10-pipe-maze/10-pipe-maze.py
@@ -6,6 +6,13 @@ import sys
 def get_maze(fh):
     return [list(line.rstrip()) for line in fh]
 
+def find_start(maze, start='S'):
+    for y,row in enumerate(maze):
+        for x,cell in enumerate(row):
+            if cell == start:
+                return (y,x)
+    return (-1,-1)
 
 if __name__ == "__main__":
-    print(get_maze(sys.stdin))
+    maze = get_maze(sys.stdin)
+    print(find_start(maze))
