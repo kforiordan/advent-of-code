1147064073b921d03b2645a32b2c4674f90197e4
Kevin O'Riordan <kevin.oriordan@heanet.ie>
2023-12-12 17:11:04 +0000

    expand universe
    Where there's an empty row or column, insert another empty r/c.

--- 2023/11-cosmic-expansions/11-cosmic-expansions.py
+++ 2023/11-cosmic-expansions/11-cosmic-expansions.py
@@ -7,5 +7,35 @@ def get_raw_universe(fh):
     return [list(row.strip()) for row in fh]
 
 
+def expand_universe(u):
+    expanded = []
+
+    def all_dots(v):
+        for d in v:
+            if d != '.':
+                return False
+        return True
+
+    # Expand rows
+    for row in u:
+        if all_dots(row):
+            expanded.append(['.' for _ in u[0]])
+        expanded.append(row)
+
+    # Expand columns
+    galaxy_count = [0 for _ in u[0]]
+    for row in u:
+        for i,cell in enumerate(row):
+            if cell != '.':
+                galaxy_count[i] += 1
+    for row in expanded:
+        for i,n in enumerate(galaxy_count):
+            if n == 0:
+                row.insert(i, '.')
+
+    return expanded
+
+
 if __name__ == "__main__":
-    print(get_raw_universe(sys.stdin))
+    universe = get_raw_universe(sys.stdin)
+    expanded_universe = expand_universe(universe)
