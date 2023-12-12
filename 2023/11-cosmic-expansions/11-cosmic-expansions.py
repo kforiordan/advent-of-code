ceb642f8b5d52ff4d2a1806265f1dd7d2a94b8e2
Kevin O'Riordan <kevin.oriordan@heanet.ie>
2023-12-12 17:57:55 +0000

    solve day 11 part 1

--- 2023/11-cosmic-expansions/11-cosmic-expansions.py
+++ 2023/11-cosmic-expansions/11-cosmic-expansions.py
@@ -29,13 +29,49 @@ def expand_universe(u):
             if cell != '.':
                 galaxy_count[i] += 1
     for row in expanded:
-        for i,n in enumerate(galaxy_count):
-            if n == 0:
-                row.insert(i, '.')
+        i = 0
+        offset = 0
+        z = len(galaxy_count)
+        while i < z:
+            if galaxy_count[i] == 0:
+                row.insert(i+offset, '.')
+                offset += 1
+            i += 1
 
     return expanded
+
+
+def manhattan(a, b):
+    (ay, ax) = a
+    (by, bx) = b
+    ydist = abs(by - ay)
+    xdist = abs(bx - ax)
+    return ydist + xdist
+
+
+def get_galaxies(u):
+    coords = []
+    for y,row in enumerate(u):
+        for x,cell in enumerate(row):
+            if cell == '#':
+                coords.append((y,x))
+    return coords
+
+
+def get_path_distances(coords):
+    path_distances = {g:[] for g in coords}
+    for i,g in enumerate(coords):
+        j = i+1
+        while j < len(coords):
+            path_distances[g].append(manhattan(g, coords[j]))
+            j += 1
+    return path_distances
 
 
 if __name__ == "__main__":
     universe = get_raw_universe(sys.stdin)
     expanded_universe = expand_universe(universe)
+    galaxy_coords = get_galaxies(expanded_universe)
+    path_distances = get_path_distances(galaxy_coords)
+
+    print("Silver: {}".format(sum(list(map(sum,list(path_distances.values()))))))
