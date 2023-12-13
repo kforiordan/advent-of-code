9863d9481b66b483882261205b67fed9054943c5
Kevin O'Riordan <kevin.oriordan@heanet.ie>
2023-12-13 22:39:56 +0000

    solve day 13 part 2 with test data
    I don't think this will work with prod data because I'm not
    differentiating between smudges (false positives) and mirrors not
    detected (false negatives).  Y'know, I'm not even gonna try this, I'll
    just do it right, see next commit.

--- 2023/13-point-of-incidence/13-point-of-incidence.py
+++ 2023/13-point-of-incidence/13-point-of-incidence.py
@@ -41,38 +41,44 @@ def silver(patterns, magic):
     return sum([pattern_score(p, magic) for p in patterns])
 
 
-def pattern_score(pattern, magic):
-    p = horizontal_pattern_score(pattern, magic)
+def gold(patterns, magic):
+    magic = 100
+    smudge_tolerance = 1
+    return sum([pattern_score(p, magic, smudge_tolerance) for p in patterns])
+
+
+def pattern_score(pattern, magic, tolerance=0):
+    p = horizontal_pattern_score(pattern, magic, tolerance)
     if p == None:
-        deep_magic_from_a_fucking_lion = 1
-        p = vertical_pattern_score(pattern, deep_magic_from_a_fucking_lion)
+        moar_magic = 1
+        p = vertical_pattern_score(pattern, moar_magic, tolerance)
         if p == None:
             print("well this is not right")
             exit(0)
     return p
 
 
-def vertical_pattern_score(pattern, magic):
-    return horizontal_pattern_score(rotate90(pattern), magic)
-
-
-def elementwise_eq(row_a, row_b):
+def elementwise_eq(row_a, row_b, tolerance=0):
+    smudge_count = 0
     for a,b in zip(row_a, row_b):
         if a == b:
+            continue
+        smudge_count += 1
+        if smudge_count <= tolerance:
             continue
         return False
     return True
 
 
-def horizontal_pattern_score(pattern, magic):
+def horizontal_pattern_score(pattern, magic, tolerance):
     prev_row = None
     for i,row in enumerate(pattern):
         if prev_row != None:
-            if elementwise_eq(prev_row, row):
+            if elementwise_eq(prev_row, row, tolerance):
                 j,k = i-1,i
                 found_nonmatching = False
                 while j >= 0 and k < len(pattern):
-                    if elementwise_eq(pattern[j],pattern[k]):
+                    if elementwise_eq(pattern[j], pattern[k], tolerance):
                         j -= 1
                         k += 1
                     else:
@@ -86,9 +92,13 @@ def horizontal_pattern_score(pattern, magic):
     return None
 
 
+def vertical_pattern_score(pattern, magic, tolerance):
+    return horizontal_pattern_score(rotate90(pattern), magic, tolerance)
+
 
 if __name__ == "__main__":
     patterns = get_ash_patterns(sys.stdin)
     magic = 100
     print("Silver: {}".format(silver(patterns, magic)))
+    print("Gold: {}".format(gold(patterns, magic)))
 
