66ac6208ad456be093aabdc8b35a36817ab15c5c
Kevin O'Riordan <kevin.oriordan@heanet.ie>
2023-12-13 22:46:33 +0000

    produce wrong answer for day 13 part 2
    Well I 'fixed' the problem I described in the previous commit, it worked
    with the test data, and failed with the prod data - answer too low.
    
    So I tried the answer generated by the previous commit - also too low.

--- 2023/13-point-of-incidence/13-point-of-incidence.py
+++ 2023/13-point-of-incidence/13-point-of-incidence.py
@@ -63,10 +63,12 @@ def elementwise_eq(row_a, row_b, tolerance=0):
     for a,b in zip(row_a, row_b):
         if a == b:
             continue
-        smudge_count += 1
-        if smudge_count <= tolerance:
-            continue
-        return False
+        else:
+            if a == '#':
+                smudge_count += 1
+                if smudge_count <= tolerance:
+                    continue
+            return False
     return True
 
 
