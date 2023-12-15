0a119e8c1807d05333559e5dbbe25529200cea79
Kevin O'Riordan <kevin.oriordan@heanet.ie>
2023-12-15 21:32:07 +0000

    Solve day 15 part 1
    That was weirdly easy

--- 2023/15-lens-library/15-lens-library.py
+++ 2023/15-lens-library/15-lens-library.py
@@ -10,9 +10,18 @@ def get_init_seq(fh):
     return steps
 
 def naive_hash(steps):
-    hash = None
-    return hash
+    hashed_steps = []
+    for s in steps:
+        v = 0
+        for c in s:
+            v += ord(c)
+            v = v * 17
+            v = v % 256
+        hashed_steps.append(v)
+
+    return hashed_steps
 
 if __name__ == "__main__":
     steps = get_init_seq(sys.stdin)
+    print("Silver: {}".format(sum(naive_hash(steps))))
 
