ab9b833f52b2c02b1a90e4834a27da08129b7222
Kevin O'Riordan <kevin@oriordan.org>
2023-12-17 21:46:46 +0000

    works on test data, fails on prod
    recursive implementation, every step involves 1 or 2 recursive calls,
    and this is too much for python.

--- 2023/16-the-floor-will-be-lava/16-the-floor-will-be-lava.py
+++ 2023/16-the-floor-will-be-lava/16-the-floor-will-be-lava.py
@@ -18,27 +18,84 @@ def get_tiles(fh):
 
 
 
-def energise(tiles, mirrors, origin, direction, energised = None):
-    (y,x) = origin[0]+direction[0], origin[1]+direction[1]
-    mirror_string = "".join(list(map(chr,[45,47,92,124])))
+def energise(tiles, mirrors, origin, direction, energised = None, depth=0):
+    if depth > 500:
+        print("Bailing out")
+        return False
 
-    in_bounds = lambda y,x: if y>=0 and y<len(tiles) and \
-        x>=0 and x<len(tiles[0])
+    (y,x) = origin[0]+direction[0], origin[1]+direction[1]
+
+    in_bounds = lambda l,w: l>=0 and l<len(tiles) and w>=0 and w<len(tiles[0])
+
     if not in_bounds(y,x):
         return False
 
     if energised == None:
         energised = {}
 
-    # oh python mode
-    if tiles[y][x] in mirror_string:
-        fjdsoifj
+    if (y,x) in energised:
+        if origin in energised[(y,x)]:
+            energised[(y,x)][origin] += 1
+            return True
+        else:
+            energised[(y,x)][origin] = 1
     else:
-        energise(tiles, mirrors, (y,x), direction, energised))
+        energised[(y,x)] = {origin: 1}
+#        print("{} -> {}    ({})".format(origin, direction, energised))
 
+    # Raw backslash breaks emacs python mode's indentation and highlighting.
+    mirror_string = "".join(['-', '|', '/', chr(92)])
+    t = tiles[y][x]
+    directions = [direction]
+    if t in mirror_string:
+        if t == '-':
+            if origin[0] != y:
+                directions = [(0,-1), (0,1)]
+        elif t == '|':
+            if origin[1] != x:
+                directions = [(-1,0), (1,0)]
+        elif t == '/':
+            if origin[0] < y:
+                directions = [(0,-1)]
+            elif origin[0] > y:
+                directions = [(0,1)]
+            elif origin[1] < x:
+                directions = [(-1,0)]
+            elif origin[1] > x:
+                directions = [(1,0)]
+            else:
+                directions = None
+        elif t == chr(92):
+            if origin[0] < y:
+                directions = [(0,1)]
+            elif origin[0] > y:
+                directions = [(0,-1)]
+            elif origin[1] < x:
+                directions = [(1,0)]
+            elif origin[1] > x:
+                directions = [(-1,0)]
+            else:
+                directions = None
 
+    if directions == None:
+        print("Exiting: {}: {} -> {}".format(t, origin, (y,x)))
+        exit(0)
+
+    for d in directions:
+        energise(tiles, mirrors, (y,x), d, energised, depth+1)
 
     return energised
+
+def print_solution(tiles, energised):
+    s = None
+    for y,row in enumerate(tiles):
+        s = []
+        for x,t in enumerate(row):
+            if (y,x) in energised:
+                s.append('#')
+            else:
+                s.append(t)
+        print("".join(s))
 
 
 if __name__ == "__main__":
@@ -47,3 +104,7 @@ if __name__ == "__main__":
     next_step = (0,1) # Moving to the right; add to current position.
     energised = energise(tiles, mirrors, start, (0,1))
     paths_seen = {}
+    print("Silver: {}".format(len(energised.keys())))
+    print_solution(tiles, energised)
+
+
